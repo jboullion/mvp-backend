da209a496a2a35155878b3cf7b84e3931a383dea
jboullion <jboullion83@gmail.com>
2021-11-14 00:41:59 -0600

    Update password error messaging

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -11,11 +11,15 @@ export class AuthCredentialsDto {
   email: string;
 
   @IsString()
-  @MinLength(8)
-  @MaxLength(32)
+  @MinLength(8, {
+    message: 'Password must be at least 8 characters long',
+  })
+  @MaxLength(32, {
+    message: 'Password must be no more than 32 characters long',
+  })
   @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
-      'Password requires as least: 1 Uppercase, 1 Lowercase, and 1 Number',
+      'Password requires as least 1 uppercase, 1 lowercase, and 1 number',
   })
   password: string;
 }
